fa940b3c6d206f3b7a5f30914beaa160de1fce92
Fidgino <Fidgino@gmail.com>
2023-01-09 15:16:10 +0200

    exo list builder à finir

---
+++ intermediaire/List Builder/assets/script.js
@@ -0,0 +1,25 @@
+let arrayFood = [];
+
+//Dom Element
+
+let buttons = document.querySelectorAll('.foodList > p');
+let foodText = document.querySelector('.foodDom');
+
+// Js
+
+function ajoutElementTexte(texte){
+    let tmp = document.createElement('span');
+    tmp.innerHTML = texte;
+    foodText.appendChild(tmp);
+}
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function() {
+        arrayFood.push(buttons[i].textContent);
+        console.log(arrayFood);
+        for(let e = 0; e < arrayFood.length; e++){
+            ajoutElementTexte(arrayFood[e]);
+         } 
+         arrayFood = [];
+    });
+}
