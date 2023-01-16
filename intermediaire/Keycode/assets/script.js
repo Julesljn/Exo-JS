2c64ca5931e56905a6fa2c0d5545cf0115d1b876
Fidgino <Fidgino@gmail.com>
2023-01-16 17:32:25 +0200

    uptade

--- intermediaire/Keycode/assets/script.js
+++ intermediaire/Keycode/assets/script.js
@@ -1,15 +1,29 @@
 const allInputs = document.querySelectorAll('input');
 const button = document.querySelector('button');
 
-for(let i = 0; i < allInputs.length; i++){
-    console.log('looping');
-    allInputs[i].addEventListener('change', () => {
-        console.log('sff')
+
+for (let i = 0; i < allInputs.length; i++) {
+    allInputs[i].addEventListener('keyup', () => {
         const regex = /[0-9]/g;
-        let found =  allInputs[i].match(regex);
-        console.log(found);
-        if(this.match(regex) !== null) {
-            console.log(found);
+        let found = regex.test(allInputs[i].value);
+        if (found) {
+            if (allInputs[i - 1]) {
+                allInputs[i - 1].disabled = true;
+            }
+            if (allInputs[i + 1]) {
+                allInputs[i + 1].disabled = false;
+                allInputs[i + 1].focus();
+            }
         }
-    })
-}
+        else {
+            if (allInputs[i - 1]) {
+                allInputs[i - 1].disabled = false;
+                allInputs[i - 1].focus();
+            }
+            if (allInputs[i + 1]) {
+                allInputs[i].value = '';
+                allInputs[i + 1].disabled = true;
+            }
+        }
+    });
+};
