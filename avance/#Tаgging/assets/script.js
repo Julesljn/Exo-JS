adebc64c15d787e27874c5aba097b5281ca14497
Fidgino <Fidgino@gmail.com>
2023-02-06 17:26:08 +0100

    add GitHub

---
+++ avance/#Tаgging/assets/script.js
@@ -0,0 +1,9 @@
+let textArea = document.querySelector('#textArea');
+
+
+textArea.addEventListener('input', () => {
+    let ashtag = '#';
+    if(textArea.index = 1){
+        console.log('frfefe');
+    }
+});
