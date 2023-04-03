f03eb44cd3b938bf75e5731b54fdbd4b45ff8d4d
Fidgino <Fidgino@gmail.com>
2023-04-03 11:04:57 +0200

    uptade

--- avance/Sequence Game/assets/script.js
+++ avance/Sequence Game/assets/script.js
@@ -1,12 +1,32 @@
-const button = document.querySelector('.button');
+const btnStart = document.querySelector('.main__btn');
+const form = document.querySelectorAll('div');
+const main = document.querySelector('.main');
+const scoreDOM = document.querySelector('.main__score');
+let previousNumber = null;
+let randomNumber = null;
+let score = 0;
+function generateRandomNumber() {
+  randomNumber = Math.floor(Math.random() * form.length); // Génère un nombre aléatoire entre 1 et 100
+    scoreDOM.innerHTML = score;
+  if (randomNumber === previousNumber) { // Vérifie si le nombre généré est égal au nombre précédent
+    return generateRandomNumber(); // Génère un nouveau nombre aléatoire en appelant la fonction récursivement
+  }
 
-const body = document.querySelector('.bodyMain');
-
-const rectangle = document.querySelector('.rectangle');
-const triangle = document.querySelector('.triangle');
-const cercle = document.querySelector('.cercle');
-
-button.addEventListener('click', () => {
-    body.style.opacity = '0.33';
-    rectangle.style.opacity = '0';
-})
+  previousNumber = randomNumber; // Met à jour le nombre précédent avec le nouveau nombre généré
+  return randomNumber; // Retourne le nouveau nombre généré
+}
+for(let i = 0; i < form.length; i++) {
+    btnStart.addEventListener('click', () => {
+    form[0].classList.add('active');
+    btnStart.disabled = true;
+    })
+    form[i].addEventListener('click', () => {
+        
+        let random = generateRandomNumber();
+        if(form[i].classList.contains('active')) {
+            form[i].classList.remove('active');
+            score += 1;
+            form[random].classList.add('active');
+        }
+    })
+}
